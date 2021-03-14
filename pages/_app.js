629d10f69fa231c800864500dd4a57d3ee354e99
Jongsun Park <jongsun250@gmail.com>
2021-03-14 23:04:28 +0000

    connect contentful & setup redux

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
+import { Provider } from "react-redux";
+import { useStore } from "../store";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const store = useStore(pageProps.initialReduxState);
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,55 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Home.module.scss";
 import Link from "next/link";
 import Date from "../components/date";
 import Image from "next/image";
 import Layout from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
+import { content } from "../content";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchContentful } from "../store/reducers/contentfulReducer";
 
-export default function Home({ allPostsData }) {
+const {
+  title,
+  author,
+  email,
+  phone,
+  location,
+  description,
+  contact,
+  skills,
+} = content;
+
+const Intro = () => (
+  <section className={utilStyles.headingMd}>
+    <p>Hi! I'm {author}. I'm self learning code and design.</p>
+    <p>{description}</p>
+  </section>
+);
+
+const Blog = ({ allPostsData }) => {
+  const [maxPost, setMaxPost] = useState(3);
+  const onLoadMore = (e) => {
+    const maxNum = allPostsData.length;
+    if (maxPost + 2 < maxNum) {
+      setMaxPost(maxPost + 2);
+    } else {
+      console.log("No post to load more");
+      e.target && e.target.classList.add("inactive");
+    }
+  };
   return (
-    <Layout home>
-      <Head>
-        <title>Create Next App</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>Hi! I'm Park. I'm self learning code and design.</p>
-        <p>
-          Thanks you for visiting my website. I can make beatiful website built
-          with Wordpress and Shopify. and design with adobe products. I really
-          design with AdobeXD for design website. Check my current works! and
-          welcome to contact me any time :)
-        </p>
-        <Link href="/works">
-          <a>My Works</a>
-        </Link>
-      </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData &&
-            allPostsData.map(({ id, date, title }) => (
+    <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+      <h2 className={utilStyles.headingLg}>Blog</h2>
+      <ul className={utilStyles.list}>
+        {allPostsData &&
+          allPostsData.map(({ id, date, title }, i) => {
+            if (maxPost < i + 1) {
+              return;
+            }
+            return (
               <li className={utilStyles.listItem} key={id}>
                 <Link href={`/posts/${id}`}>
                   <a>{title}</a>
@@ -39,9 +59,92 @@ export default function Home({ allPostsData }) {
                   <Date dateString={date} />
                 </small>
               </li>
-            ))}
+            );
+          })}
+        <button className={styles.btnLoadmore} onClick={onLoadMore}>
+          Load More
+        </button>
+      </ul>
+    </section>
+  );
+};
+
+const Skills = () => (
+  <section>
+    <h2>Skills</h2>
+    <div>
+      {skills.map(({ title, description }) => {
+        return (
+          <div key={title}>
+            <h3>{title}</h3>
+            <p>{description}</p>
+          </div>
+        );
+      })}
+    </div>
+  </section>
+);
+
+const Contact = () => (
+  <section>
+    <h2>Contact</h2>
+    <p>{contact}</p>
+    location: {location}
+    <br />
+    phone: {phone}
+    <br />
+    email: {email}
+  </section>
+);
+
+const Work = () => {
+  const { loading, error, data } = useSelector((state) => state.contentful);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    fetchContentful(dispatch);
+  }, []);
+  return (
+    <section className={styles.work}>
+      <h3>Work</h3>
+      <div>
+        <ul>
+          {loading ? (
+            <p>loading...</p>
+          ) : (
+            data.map((p) => (
+              <div key={p.fields.title}>
+                <Image
+                  width="300"
+                  height="300"
+                  src={`https:${p.fields.thumbnail.fields.file.url}`}
+                />
+                <h3>{p.fields.title}</h3>
+                <p>{p.fields.description}</p>
+                {p.fields.url && <Link href={p.fields.url}>LIVE</Link>}
+                <br />
+                {p.fields.body && (
+                  <Link href={`/projects/${p.sys.id}`}>DETAIL</Link>
+                )}
+              </div>
+            ))
+          )}
         </ul>
-      </section>
+      </div>
+    </section>
+  );
+};
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <Intro />
+      <Work />
+      <Skills />
+      <Blog allPostsData={allPostsData} />
+      <Contact />
     </Layout>
   );
 }
