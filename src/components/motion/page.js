dd99d0bb82c2505dc2eeeecb25784db94601cb2b
Jongsun Park <jongsun250@gmail.com>
2021-03-21 20:29:40 +0000

    change page variants

--- src/components/motion/page.js
+++ src/components/motion/page.js
@@ -2,7 +2,7 @@ import { motion } from "framer-motion";
 
 const pageVariant = {
   hidden: { opacity: 0 },
-  visible: { opacity: 1, transition: { delay: 1.5, duration: 1.5 } },
+  visible: { opacity: 1, transition: { delay: 0, duration: 0.3 } },
   exit: {
     x: "-100vw",
     transition: { ease: "easeInOut" },
